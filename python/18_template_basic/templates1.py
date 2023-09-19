72a68fcf4672080b9867716d1aac189f80c472f1
Erno Hentonen <erno.hentonen@opiframe.com>
2023-09-19 11:02:51 +0300

    flask 2

---
+++ python/18_template_basic/templates1.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html",title="Welcome",username="Erno")
+    
+@app.route("/if")
+def if_example():
+    return render_template("if.html",title="If example",username="Erno")
+ 
+@app.route("/if/<username>")
+def if_username(username):
+    name = escape(username)
+    return render_template("if.html",title="If example 2",username=name)
+    
+app.run("127.0.0.1", port=8080)
