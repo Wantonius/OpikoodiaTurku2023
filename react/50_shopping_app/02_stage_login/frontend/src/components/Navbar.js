3e74095b0adc7455ee5b2e79d26c6ee565e6e354
Erno Hentonen <erno.hentonen@opiframe.com>
2023-10-11 14:28:04 +0300

    login

--- react/50_shopping_app/02_stage_login/frontend/src/components/Navbar.js
+++ react/50_shopping_app/02_stage_login/frontend/src/components/Navbar.js
@@ -1,20 +1,33 @@
 import {Link} from 'react-router-dom'
 
 const Navbar = (props) => {
-
-	return(
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
-			<ul className="navbar-nav">
-				<li className="nav-item" style={{marginLeft:10}}>
-					<Link className="nav-link" to="/">Shopping List</Link>
-				</li>
-				<li className="nav-item" style={{marginLeft:10}}>
-					<Link className="nav-link" to="/form">Add new item</Link>
-				</li>
-			</ul>
-		</nav>
-	)
+	if(props.isLogged) {
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
+				<ul className="navbar-nav">
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link className="nav-link" to="/">Shopping List</Link>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link className="nav-link" to="/form">Add new item</Link>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<p style={{color:"blue"}}>Logged in as {props.user}</p>
+					</li>
+					<li className="nav-item" style={{marginLeft:10}}>
+						<Link className="nav-link" to="/" onClick={props.logout}>Logout</Link>
+					</li>
+				</ul>
+			</nav>
+		)
+	} else {
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<p className="navbar-brand" style={{marginLeft:10}}>Shopping App</p>
+			</nav>
+		)
+	}
 }
 
 export default Navbar;
