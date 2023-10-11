67a136dd2116737f546c355ffcfd8cad054e3a8a
Erno Hentonen <erno.hentonen@opiframe.com>
2023-10-11 09:47:48 +0300

    login

--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -11,6 +11,46 @@ app.use(express.json());
 
 let port = process.env.PORT || 3001;
 
+//LOGIN DATABASES
+
+let registeredUsers = [];
+let loggedSessions = [];
+const time_to_live_diff = 3600000;
+
+//LOGIN MIDDLEWARES
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({"Message":"Username already in use"});
+		}
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			console.log(err);
+			return res.status(500).json({"Message":"Internal server error"})
+		}
+		let user = {
+			username:req.body.username,
+			password:hash
+		}
+		console.log(user);
+		registeredUsers.push(user);
+		return res.status(201).json({"Message":"Register success"});
+	})
+})
+
 app.use("/api",shoppingroute);
 
 console.log("Running in port",port);
