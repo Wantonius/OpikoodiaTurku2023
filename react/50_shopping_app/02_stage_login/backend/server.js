92e47a2ccf0a571c6c28dbef68ebb8ea3b866fd3
Erno Hentonen <erno.hentonen@opiframe.com>
2023-10-11 10:23:53 +0300

    login

--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -19,6 +19,11 @@ const time_to_live_diff = 3600000;
 
 //LOGIN MIDDLEWARES
 
+const createToken = () => {
+	let token = crypto.randomBytes(64);
+	return token.toString("hex");
+}
+
 //LOGIN API
 
 app.post("/register",function(req,res) {
@@ -49,6 +54,42 @@ app.post("/register",function(req,res) {
 		registeredUsers.push(user);
 		return res.status(201).json({"Message":"Register success"});
 	})
+})
+
+app.post("/login",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({"Message":"Bad Request"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			bcrypt.compare(req.body.password,registeredUsers[i].password,function(err,success) {
+				if(err) {
+					console.log(err);
+					return res.status(500).json({"Message":"Internal server error"});
+				}
+				if(!success) {
+					return res.status(401).json({"Message":"Unauthorized"});
+				}
+				let token = createToken();
+				let now = Date.now();
+				let session = {
+					"token":token,
+					"user":req.body.username,
+					"ttl":now+time_to_live_diff
+				}
+				loggedSessions.push(session);
+				return res.status(200).json({"token":token});
+			})
+			return;
+		}
+	}
+	return res.status(401).json({"Message":"Unauthorized"});
 })
 
 app.use("/api",shoppingroute);
