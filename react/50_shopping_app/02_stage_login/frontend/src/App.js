27501ad0f1106fec54d23873d8a98791dcf06f61
Erno Hentonen <erno.hentonen@opiframe.com>
2023-10-11 14:17:39 +0300

    login

--- react/50_shopping_app/02_stage_login/frontend/src/App.js
+++ react/50_shopping_app/02_stage_login/frontend/src/App.js
@@ -295,16 +295,44 @@ function App() {
 		})
 	}
 	
-	return (
-		<div className="App">
-			<Navbar/>
-			<Routes>
-				<Route path="/" element={<ShoppingList list={state.list} removeItem={removeItem} editItem={editItem}/>}/>
-				<Route path="/form" element={<ShoppingForm addItem={addItem}/>}/>
-				<Route path="*" element={<Navigate to="/"/>}/>
-			</Routes>
-		</div>
-	);
+	//RENDERING
+	
+	let message = <></>
+	if(state.loading) {
+		message = <h4>Loading...</h4>
+	}
+	if(state.error) {
+		message = <h4>{state.error}</h4>
+	}
+	if(state.isLogged) {
+		return (
+			<div className="App">
+				<Navbar token={state.token} user={state.user} isLogged={state.isLogged} logout={logout}/>
+				<div style={{height:35,textAlign:"center"}}>
+					{message}
+				</div>
+				<Routes>
+					<Route path="/" element={<ShoppingList list={state.list} removeItem={removeItem} editItem={editItem}/>}/>
+					<Route path="/form" element={<ShoppingForm addItem={addItem}/>}/>
+					<Route path="*" element={<Navigate to="/"/>}/>
+				</Routes>
+			</div>
+		);
+	} else {
+		return (
+			<div className="App">
+				<Navbar token={state.token} user={state.user} isLogged={state.isLogged} logout={logout}/>
+				<div style={{height:35,textAlign:"center"}}>
+					{message}
+				</div>
+				<Routes>
+					<Route path="/" element={<LoginPage register={register} login={login} setError={setError}/>}/>
+					<Route path="*" element={<Navigate to="/"/>}/>
+				</Routes>
+			</div>
+		);		
+		
+	}
 }
 
 export default App;
