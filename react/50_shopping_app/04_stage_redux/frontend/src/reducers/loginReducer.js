c9c5137987514af08be499e4d8eaa17bb8c055ea
Erno Hentonen <erno.hentonen@opiframe.com>
2023-10-16 12:32:06 +0300

    redux

--- react/50_shopping_app/04_stage_redux/frontend/src/reducers/loginReducer.js
+++ react/50_shopping_app/04_stage_redux/frontend/src/reducers/loginReducer.js
@@ -22,7 +22,75 @@ const saveToStorage = (state) => {
 const initialState = getInitialState();
 
 const loginReducer = (state = initialState,action) => {
-	
+	console.log("loginReducer, action",action);
+	let tempState = {
+		...state
+	}
+	switch(action.type) {
+		case actionConstants.LOADING:
+			return {
+				...state,
+				error:"",
+				loading:true
+			}
+		case actionConstants.STOP_LOADING:
+			return {
+				...state,
+				loading:false
+			}
+		case actionConstants.REGISTER_SUCCESS:
+			tempState = {
+				...state,
+				error:"Register Success"
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGIN_SUCCESS:
+			tempState = {
+				...state,
+				token:action.token,
+				isLogged:true
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGOUT_SUCCESS:
+			tempState = {
+				isLogged:false,
+				loading:false,
+				token:"",
+				error:"",
+				user:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.REGISTER_FAILED:
+		case actionConstants.LOGIN_FAILED:
+			tempState = {
+				...state,
+				error:action.error;
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGOUT_FAILED:
+			tempState = {
+				isLogged:false,
+				loading:false,
+				token:"",
+				error:action.error,
+				user:""
+			}
+			saveToStorage(tempState);
+			return tempState;		
+		case actionConstants.SET_USERNAME:
+			tempState = {
+				...state,
+				user:action.user
+			}
+			saveToStorage(tempState);
+			return tempState;
+		default:
+			return state;
+	}
 }
 
 export default loginReducer;
